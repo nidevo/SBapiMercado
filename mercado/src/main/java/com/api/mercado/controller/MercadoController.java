7d0789edf325b6c36fc94548c4d3518ae67b338d
Claudemir <teixeiravitorinoc8@gmail.com>
2024-09-26 17:06:09 -0300

    Métodos agora possuem catch.

--- mercado/src/main/java/com/api/mercado/controller/MercadoController.java
+++ mercado/src/main/java/com/api/mercado/controller/MercadoController.java
@@ -3,6 +3,7 @@ package com.api.mercado.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.api.mercado.model.Produto;
 import com.api.mercado.repository.MercadoRepository;
@@ -27,21 +29,40 @@ public class MercadoController {
 	@GetMapping
 	public List<Produto> listar(){
 		return repository.findAll();
+		
+	}
+	
+	@GetMapping("/{id}")
+	public Produto listarid(@PathVariable Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado com o ID: " + id));
 	}
 	
 	@PostMapping
 	public void cadastrar(@RequestBody Produto produto) {
 		repository.save(produto);
+		
 	}
 	
 	@PutMapping("/{id}")
 	public void atualizar(@PathVariable Long id, @RequestBody Produto produto) {
-		produto.setId(id);
-		repository.save(produto);
+		
+	    repository.findById(id)
+	        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado com o ID: " + id));
+
+	    
+	    produto.setId(id);
+	    repository.save(produto);
+		
 	}
 	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable Long id) {
-		repository.deleteById(id);
+		Produto produto = repository.findById(id)
+		.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado para exclusão com o ID: " + id));
+		
+		repository.delete(produto);
 	}
+	
+	
 	
 }
